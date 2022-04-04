3c0906357b507cd210329859d23deb2eb316f126
KyleCrawford <kyle.g.crawford@gmail.com>
2022-04-04 13:53:06 -0600

    Installed PagedList
    Installed PagedList

--- Controllers/BlogController.cs
+++ Controllers/BlogController.cs
@@ -3,15 +3,59 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using BlogMe.Models;
+using PagedList;
 
 namespace BlogMe.Controllers
 {
     public class BlogController : Controller
     {
-        // GET: Blog
-        public ActionResult Index()
+        private ApplicationDbContext _context;
+
+        public BlogController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+        // GET: Blogs
+        // Show all blogs (paginated)
+        public ActionResult Index(int? page)
+        {
+            List<Blog> blogs = _context.Blogs.ToList();
+
+            int pageSize = 2;
+            int pageNumber = (page ?? 1);
+
+            return View(blogs.ToPagedList(pageNumber, pageSize));
+        }
+
+            // View Blog
+            // Show one blog in reading format
+            public ActionResult Read(int id)
+        {
+            Blog blog = _context.Blogs.Find(id);
+            return View(blog);
+        }
+
+        // New Blog
+        // Create a blog, must be logged in
+        public ActionResult New()
         {
             return View();
         }
+
+        // Edit
+        // Edit an existing blog
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // Delete - Can just put this on the Edit page. Might need a function
+
     }
 }
