ffa3d5291e6e70a60310d4566d1dbccae06a946f
KyleCrawford <kyle.g.crawford@gmail.com>
2022-04-04 14:10:04 -0600

    Created Index ViewModel
    Created Index ViewModel
    named ViewAllBlogsViewModel

---
+++ ViewModels/ViewAllBlogsViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using BlogMe.Models;
+
+namespace BlogMe.ViewModels
+{
+    public class ViewAllBlogsViewModel
+    {
+        public PagedList.IPagedList<BlogMe.Models.Blog> BlogList { get; set; }
+        public ApplicationUser CurrentUser { get; set; }
+    }
+}
