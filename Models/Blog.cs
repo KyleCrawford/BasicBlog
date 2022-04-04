a72969cc5a2e04011926ef4a187b140d30d32b19
KyleCrawford <kyle.g.crawford@gmail.com>
2022-04-04 12:04:45 -0600

    Created Blog Class
    Created Blog Class

---
+++ Models/Blog.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace BlogMe.Models
+{
+    public class Blog
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Title { get; set; }
+
+
+        public DateTime CreatedOn { get; set; }
+
+        [Display(Name = "Your Blog Text")]
+        [Required]
+        public string BlogText { get; set; }
+
+        public string BlogOwnerId { get; set; }
+        public ApplicationUser BlogOwner { get; set; }
+    }
+}
